4854767fc9df79dcde3e44c97d3d82932d5f831f
Jan Betley <jan.betley@gmail.com>
2023-06-28 15:08:16 +0000

    looks like this "works", i.e. mouse goes to cheese a tiny bit more often

--- compare_channels.py
+++ compare_channels.py
@@ -8,7 +8,7 @@ import torch as t
 from procgen_tools.imports import load_model
 from procgen_tools import maze, visualization
 
-from tools import maze_str_to_grid, PolicyWithRelu3Mod, get_seed
+from tools import maze_str_to_grid, PolicyWithRelu3Mod, get_seed_with_decision_square, rollout, compare_policies
 from procgen_tools.rollout_utils import rollout_video_clip, get_predict
 # %%
 
@@ -129,75 +129,44 @@ print(channels.topk(10))
 # TEST 1 - channels selected for this particular maze
 def get_mod_func(channels):
     def modify_relu3(x):
-        x[:, channels] *= 0
+        x[:, channels] *= 4
     return modify_relu3
 
-# TOP_CHANNELS = 16    
-# mod_func = get_mod_func(channels.topk(TOP_CHANNELS).indices)
-# modified_policy = PolicyWithRelu3Mod(policy, mod_func)
+example_2_4_channels = [6, 7, 14, 17, 21, 30, 35, 43, 53, 71, 73, 80, 96, 101, 110, 112, 121, 123, 124]
 
+mod_func = get_mod_func(example_2_4_channels)
+modified_policy = PolicyWithRelu3Mod(policy, mod_func)
+
+# %%
 # vf_1_modified = visualization.vector_field(venv_1, modified_policy)
 # vf_2_modified = visualization.vector_field(venv_2, modified_policy)
 
 # visualization.plot_vfs(vf_1, vf_1_modified)
 # visualization.plot_vfs(vf_2, vf_2_modified)
 
-
 # %%
-# TEST 2 - use predefined channels
-# Selected from examples 2-4. Expected to works on them.
-example_2_4_channels = [6, 7, 14, 17, 21, 30, 35, 43, 53, 71, 73, 80, 96, 101, 110, 112, 121, 123, 124]
 
-mod_func = get_mod_func(example_2_4_channels)
-modified_policy = PolicyWithRelu3Mod(policy, mod_func)
-
-vf_1_modified = visualization.vector_field(venv_1, modified_policy)
-vf_2_modified = visualization.vector_field(venv_2, modified_policy)
-
-visualization.plot_vfs(vf_1, vf_1_modified)
-visualization.plot_vfs(vf_2, vf_2_modified)
-
-# %%
-def get_seed_with_decision_square(size: int) -> int:
-    assert size % 2, "size must be an odd number"
-
-    while True:
-        seed = np.random.randint(100000000)
-        while maze.get_inner_grid_from_seed(seed=seed).shape[0] != size:
-            seed = np.random.randint(100000000)
-
-        venv = maze.create_venv(num=1, start_level=seed, num_levels=1)
-        state_bytes = venv.env.callmethod("get_state")[0]
-        
-        if maze.maze_has_decision_square(state_bytes):
-            return seed
-
-def rollout(policy, seed: int, num_steps: int = 256) -> bool:
-    venv = maze.create_venv(num = 1, start_level=seed, num_levels=1)
-    
-    obs = venv.reset()
-    for i in range(num_steps):
-        obs = t.tensor(obs, dtype=t.float32)
-        with t.no_grad():
-            categorical, value = policy(obs)
-        
-        action = categorical.sample().numpy()
-        # action = categorical.logits.argmax().unsqueeze(0).numpy()
-        obs, rewards, dones, infos = venv.step(action)
-        
-        if dones[0]:
-            return True
-    return False
-
-for i in range(100):
+for i in range(1000):
     seed = get_seed_with_decision_square(13)
     result_orig = rollout(policy, seed)
     result_modified = rollout(modified_policy, seed)
-    print(seed, result_orig, result_modified)
+    msg = [i]
+    if result_orig != result_modified:
+        msg += [seed, result_orig, result_modified]
+        if result_modified:
+            msg.append(":-)")
+    else:
+        msg.append(result_orig)
+    print(*msg)
 
 # %%
-# predict = get_predict(policy)
-# x = rollout_video_clip(predict, seed)
+    
+compare_policies(92769969, policy, modified_policy)
 
-# x[1].write_videofile('/root/t1.mp4')
+
+# %%
+predict = get_predict(modified_policy)
+x = rollout_video_clip(predict, 47643765)
+
+x[1].write_videofile('/root/t1.mp4')
 # %%
