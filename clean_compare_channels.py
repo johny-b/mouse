961b0e45225a768d94af067ba4300dbd1debb9cd
Jan Betley <jan.betley@gmail.com>
2023-07-01 21:24:21 +0200

    some small changes

--- clean_compare_channels.py
+++ clean_compare_channels.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch as t
 
 from procgen_tools.imports import load_model
-from procgen_tools import visualization
+from procgen_tools import visualization, maze
 
 from custom_channels import get_venvs
 
@@ -19,8 +19,14 @@ policy, hook = load_model()
 
 # %%
 venvs, labels = get_venvs('cheese')
+print(maze.state_from_venv(venvs[1][0]).inner_grid().shape)
 
-example_ix = 0
+from matplotlib import pyplot as plt
+obs = venvs[0][0].reset()[0]
+plt.imshow(t.from_numpy(obs).permute(1,2,0))
+# %%
+
+example_ix = 4
 print(labels[example_ix])
 visualization.visualize_venv(venvs[example_ix][0], render_padding=False)
 visualization.visualize_venv(venvs[example_ix][1], render_padding=False)
@@ -49,15 +55,36 @@ raw_data = get_raw_data(venvs)
 assert raw_data.shape == (venv_cnt, 2, 128, 8, 8)
                     
 # %%
-
 channel_sums = raw_data.sum((-1,-2))
 assert channel_sums.shape == (venv_cnt, 2, 128)
+
 # %%
 channel_diff = (channel_sums[:, 0] - channel_sums[:, 1]).abs()
 assert channel_diff.shape == (venv_cnt, 128)
+
 # %%
-channel = 73
+channel = 21
 for venv_data, label in zip(channel_sums, labels):
     print(channel, label, venv_data[:,channel].round().to(t.int).tolist())
 
+# %%
+#   TODO: fix plotting here
+def plot_channel(ax, channel):
+    from matplotlib import pyplot as plt
+    # plt.figure(figsize=(3.8,3.8))
+    
+    c = channel.unsqueeze(2)
+    x = ax.imshow(c, vmin=0, vmax=8, alpha=0.00)
+    plt.colorbar(x)
+    plt.axis('off')
+    plt.show()
+
+for example_ix in range(16):
+    for version_ix in range(2):
+        venv = venvs[example_ix][version_ix]
+        img, ax = visualization.visualize_venv(venv)
+        
+        plot_channel(ax, raw_data[example_ix][version_ix][21])
+        break
+    break
 # %%
